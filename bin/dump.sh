5b35903ee960d089ee4983fe8ef48c1e28872e2a
Felix Hummel <root@felixhummel.de>
2017-04-13 23:51:53 +0200

    simpler dump
    docker-compose exec works as expected nowadays

--- bin/dump.sh
+++ bin/dump.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 set -euo pipefail
 
-dumpfile=$1
-[[ ! -f $dumpfile ]]
-
-container=$(docker-compose ps | grep postgres | awk '{ print $1 }')
-docker exec -i $container pg_dump --format=c --username=postgres pad > $dumpfile
+docker-compose exec postgres pg_dump --format=c --username=postgres pad
 
